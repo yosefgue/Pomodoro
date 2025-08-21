e6524945aaac6bb34fffebb3545fd9c08814169f
yosefgue <yosefgue6@gmail.com>
2025-08-21 02:07:45 +0100

    todos can be added now, plus added todo options (still no functionality for options)

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -2,15 +2,41 @@ import { useState } from "react"
 import styles from "./todo.module.css"
 
 export default function Todo() {
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handleClick = () => {
+        if (inputValue == "") return;
+        setTodos([...todos, inputValue])
+        setInputValue("")
+    }
+
     return(
         <div className={styles.maincontainer}>
             <div className={styles.titlecontainer}>
-                <input className={styles.input} type="text" placeholder="add todo" ></input>
-                <span><button className={styles.addbutton} type="button"><img src="/img/plus.svg" alt="add" /></button></span>
+                <input className={styles.input} type="text" placeholder="add todo" value={inputValue} onChange={(e) => setInputValue(e.target.value)} ></input>
+                <span><button className={styles.addbutton} type="button" onClick={handleClick}><img src="/img/plus.svg" alt="add" /></button></span>
             </div>
-            <div calssName={styles.todocontainer}>
-                <div className={styles.todo}>eat fish</div>
-            </div>
+            <AllTodo todolist={todos}/>
+        </div>
+    )
+}
+
+function OneTodo({text}) {
+    return(
+        <div className={styles.onetodocontainer}>
+            <div className={styles.todo}>{text}</div>
+            <button className={styles.options}><img src="/img/options.svg" alt="options"/></button>
+        </div>
+    )
+}
+
+function AllTodo({ todolist }) {
+    return(
+        <div className={styles.alltodocontainer}>
+            {todolist.map((todo) => (
+                <OneTodo text={todo}/>
+            ))}
         </div>
     )
 }
