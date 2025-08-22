84a2ceefcc320f8e54792272f0dd7bcf3e4aaabf
yosefgue <yosefgue6@gmail.com>
2025-08-22 03:19:07 +0100

    can delete todo, can click away for menu to disappear

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import styles from "./todo.module.css"
 
 export default function Todo() {
@@ -7,8 +7,12 @@ export default function Todo() {
 
     const handleClick = () => {
         if (inputValue == "") return;
-        setTodos([...todos, inputValue])
+        setTodos([...todos, {id: Date.now(), text: inputValue}])
         setInputValue("")
+    }
+
+    const handleDelete = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
     }
 
     return(
@@ -17,25 +21,50 @@ export default function Todo() {
                 <input className={styles.input} type="text" placeholder="add todo" value={inputValue} onChange={(e) => setInputValue(e.target.value)} ></input>
                 <span><button className={styles.addbutton} type="button" onClick={handleClick}><img src="/img/plus.svg" alt="add" /></button></span>
             </div>
-            <AllTodo todolist={todos}/>
+            <AllTodo todolist={todos} handleDelete={handleDelete}/>
         </div>
     )
 }
 
-function OneTodo({text}) {
+function OneTodo({text , id, handleDelete}) {
+    const [showMenu, setShowMenu] = useState(false);
+    const handleClick = () => {
+        setShowMenu(!showMenu)
+    }
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) setShowMenu(false);
+        }
+        document.addEventListener("mousedown", handleClickOutside)
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [])
+    
     return(
-        <div className={styles.onetodocontainer}>
+        <div className={styles.onetodocontainer} ref={ref}>
             <div className={styles.todo}>{text}</div>
-            <button className={styles.options}><img src="/img/options.svg" alt="options"/></button>
+            <div className={styles.optionsbutton} onClick={handleClick}>
+                <img src="/img/options.svg" alt="options"/>
+                {showMenu && (
+                <div className={styles.optionsMenu}>
+                    <div className={`${styles.option} ${styles.a}`}>done</div>
+                    <div className={`${styles.option} ${styles.b}`} onClick={() => handleDelete(id)}>delete</div>
+                </div>
+                )}
+            </div>
         </div>
     )
 }
 
-function AllTodo({ todolist }) {
+function AllTodo({ todolist, handleDelete }) {
+
     return(
         <div className={styles.alltodocontainer}>
             {todolist.map((todo) => (
-                <OneTodo text={todo}/>
+                <OneTodo key={todo.id} text={todo.text} id={todo.id} handleDelete={handleDelete}/>
             ))}
         </div>
     )
